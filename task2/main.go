86d092454ac7dd06db0d7800d90169f4cc2b0caa
Andrew <ozzy.vob@mail.ru>
2021-10-26 19:54:06 +0300

    task2 revisioned

--- task2/main.go
+++ task2/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,29 +19,36 @@ var answer string
 var err error
 
 func askUserValues() {
+	var scanner = bufio.NewScanner(os.Stdin)
 
 	for {
 	START1:
-		fmt.Println("Enter 'a' side of the 1-st letter")
-		_, err = fmt.Scan(&letter1.SideA)
+		fmt.Println("Enter 'a' side length of the 1-st letter")
+		scanner.Scan()
+		letter1.SideA, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			goto START1
 		}
+
 	START2:
-		fmt.Println("Enter 'b' side of  the 1-st letter")
-		_, err = fmt.Scan(&letter1.SideB)
+		fmt.Println("Enter 'b' side length of the 1-st letter")
+		scanner.Scan()
+		letter1.SideB, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			goto START2
 		}
+
 	START3:
-		fmt.Println("Enter 'c' side of the 2-nd letter")
-		_, err = fmt.Scan(&letter2.SideA)
+		fmt.Println("Enter 'c' side length of the 2-nd letter")
+		scanner.Scan()
+		letter2.SideA, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			goto START3
 		}
 	START4:
-		fmt.Println("Enter 'd' side of the 2-nd letter")
-		_, err = fmt.Scan(&letter2.SideB)
+		fmt.Println("Enter 'd' side length of the 2-nd letter")
+		scanner.Scan()
+		letter2.SideB, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			goto START4
 		}
