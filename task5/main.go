7644112207d4f154ee6f2262011fdb7bf566e726
Andrew <ozzy.vob@mail.ru>
2021-11-02 00:05:21 +0200

    task5 TESTS

--- task5/main.go
+++ task5/main.go
@@ -7,57 +7,55 @@ import (
 	"io"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var (
 	tillTwenty = map[string]string{
 		"0":  "",
-		"1":  "один",
-		"2":  "два",
-		"3":  "три",
-		"4":  "четыре",
-		"5":  "пять",
-		"6":  "шесть",
-		"7":  "семь",
-		"8":  "восемь",
-		"9":  "девять",
-		"10": "десять",
-		"11": "одиннадцать",
-		"12": "двенадцать",
-		"13": "тринадцать",
-		"14": "четырнадцать",
-		"15": "пятнадцать",
-		"16": "шестнадцать",
-		"17": "семнадцать",
-		"18": "восемнадцать",
-		"19": "девятнадцать",
+		"1":  "один ",
+		"2":  "два ",
+		"3":  "три ",
+		"4":  "четыре ",
+		"5":  "пять ",
+		"6":  "шесть ",
+		"7":  "семь ",
+		"8":  "восемь ",
+		"9":  "девять ",
+		"10": "десять ",
+		"11": "одиннадцать ",
+		"12": "двенадцать ",
+		"13": "тринадцать ",
+		"14": "четырнадцать ",
+		"15": "пятнадцать ",
+		"16": "шестнадцать ",
+		"17": "семнадцать ",
+		"18": "восемнадцать ",
+		"19": "девятнадцать ",
 	}
 
 	tillHundred = map[string]string{
-		"2": "двадцать",
-		"3": "тридцать",
-		"4": "сорок",
-		"5": "пятьдесят",
-		"6": "шестьдесят",
-		"7": "семьдесят",
-		"8": "восемьдесят",
-		"9": "девяносто",
+		"2": "двадцать ",
+		"3": "тридцать ",
+		"4": "сорок ",
+		"5": "пятьдесят ",
+		"6": "шестьдесят ",
+		"7": "семьдесят ",
+		"8": "восемьдесят ",
+		"9": "девяносто ",
 	}
 
 	hundreds = map[string]string{
 		"0": "",
-		"1": "сто",
-		"2": "двести",
-		"3": "триста",
-		"4": "четыреста",
-		"5": "пятьсот",
-		"6": "шестьсот",
-		"7": "семьсот",
-		"8": "восемьсот",
-		"9": "девятьсот",
+		"1": "сто ",
+		"2": "двести ",
+		"3": "триста ",
+		"4": "четыреста ",
+		"5": "пятьсот ",
+		"6": "шестьсот ",
+		"7": "семьсот ",
+		"8": "восемьсот ",
+		"9": "девятьсот ",
 	}
-	forRead []string
 )
 
 func userInput(r io.Reader) (string, error) {
@@ -72,6 +70,7 @@ func userInput(r io.Reader) (string, error) {
 // The next function devides input into pieces, depends on it length. It should be comfortable to work next.
 
 func inputOperations(input string) ([]string, error) {
+	var forRead []string
 	_, err := strconv.Atoi(input)
 	if err != nil {
 		return forRead, errors.New("use only numbers in input")
@@ -133,23 +132,23 @@ func logic(piece string) string {
 	chunk = addToThree(chunk)
 
 	// writes down quantity of hundreds
-	toWrite += hundreds[string(chunk[0])] + " "
+	toWrite += hundreds[string(chunk[0])]
 	// trick to separate teens
 	if chunk[1] == '1' {
 		toWrite += tillTwenty[chunk[1:]]
 		return toWrite
 	}
 	// fills the string with other numbers
-	toWrite += tillHundred[string(chunk[1])] + " " + tillTwenty[string(chunk[2])]
-	return strings.TrimSpace(toWrite)
+	toWrite += tillHundred[string(chunk[1])] + tillTwenty[string(chunk[2])]
+	return toWrite
 }
 
 func logicThousand(piece string) string {
-	tillTwenty["1"] = "одна"
-	tillTwenty["2"] = "две"
-	result := logic(piece) + " "
-	tillTwenty["1"] = "один"
-	tillTwenty["2"] = "два"
+	tillTwenty["1"] = "одна "
+	tillTwenty["2"] = "две "
+	result := logic(piece)
+	tillTwenty["1"] = "один "
+	tillTwenty["2"] = "два "
 	return result
 }
 
@@ -158,9 +157,9 @@ func addEnding(piece string) string {
 	chunk = addToThree(chunk)
 
 	switch {
-	case string(chunk[2]) == "1":
+	case string(chunk[2]) == "1" && string(chunk[1]) != "1":
 		return " "
-	case string(chunk[2]) == "2" || string(chunk[2]) == "3" || string(chunk[2]) == "4":
+	case string(chunk[2]) == "2" && string(chunk[1]) != "1" || string(chunk[2]) == "3" && string(chunk[1]) != "1" || string(chunk[2]) == "4" && string(chunk[1]) != "1":
 		return "а "
 	default:
 		return "ов "
@@ -172,9 +171,9 @@ func addEndingThousand(piece string) string {
 	chunk = addToThree(chunk)
 
 	switch {
-	case string(chunk[2]) == "1":
+	case string(chunk[2]) == "1" && string(chunk[1]) != "1":
 		return "а "
-	case string(chunk[2]) == "2" || string(chunk[2]) == "3" || string(chunk[2]) == "4":
+	case string(chunk[2]) == "2" && string(chunk[1]) != "1" || string(chunk[2]) == "3" && string(chunk[1]) != "1" || string(chunk[2]) == "4" && string(chunk[1]) != "1":
 		return "и "
 	default:
 		return " "
@@ -194,16 +193,16 @@ func completePieces(input []string) string {
 		result = "ноль"
 		return result
 	}
-	if input[0] != "000" {
-		result += logic(input[0]) + " триллион" + addEnding(input[0])
+	if input[0] != "000" && input[0] != "00" && input[0] != "0" {
+		result += logic(input[0]) + "триллион" + addEnding(input[0])
 	}
-	if input[1] != "000" {
-		result += logic(input[1]) + " миллиард" + addEnding(input[1])
+	if input[1] != "000" && input[1] != "00" && input[1] != "0" {
+		result += logic(input[1]) + "миллиард" + addEnding(input[1])
 	}
-	if input[2] != "000" {
-		result += logic(input[2]) + " миллион" + addEnding(input[2])
+	if input[2] != "000" && input[2] != "00" && input[2] != "0" {
+		result += logic(input[2]) + "миллион" + addEnding(input[2])
 	}
-	if input[3] != "000" {
+	if input[3] != "000" && input[3] != "00" && input[3] != "0" {
 		result += logicThousand(input[3]) + "тысяч" + addEndingThousand(input[3])
 	}
 	if input[4] != "000" {
---
+++ task5/main_test.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestInputOperations(t *testing.T) {
+	testTable := []struct {
+		input       string
+		expected    []string
+		expectedErr error
+	}{
+		{
+			input:       "2223fdsf232",
+			expected:    []string{},
+			expectedErr: errors.New("use only numbers in input"),
+		},
+		{
+			input:       "2223232543543543321",
+			expected:    []string{},
+			expectedErr: errors.New("cannot handle your number"),
+		},
+		{
+			input:       "32421",
+			expected:    []string{"000", "000", "000", "32", "421"},
+			expectedErr: nil,
+		},
+		{
+			input:       "43253452321",
+			expected:    []string{"000", "43", "253", "452", "321"},
+			expectedErr: nil,
+		},
+		{
+			input:       "0",
+			expected:    []string{"000", "000", "000", "000", "0"},
+			expectedErr: nil,
+		},
+		{
+			input:       "00",
+			expected:    []string{"000", "000", "000", "000", "00"},
+			expectedErr: nil,
+		},
+	}
+
+	for _, testCase := range testTable {
+		result, err := inputOperations(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if !reflect.DeepEqual(err, testCase.expectedErr) {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expectedErr, err)
+		}
+		if !reflect.DeepEqual(err, testCase.expectedErr) {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+		}
+	}
+}
+
+func TestAddToThree(t *testing.T) {
+	testTable := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "2",
+			expected: "002",
+		},
+		{
+			input:    "31",
+			expected: "031",
+		},
+		{
+			input:    "564",
+			expected: "564",
+		},
+	}
+
+	for _, testCase := range testTable {
+		result := addToThree(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+		}
+	}
+}
+
+func TestLogic(t *testing.T) {
+	testTable := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "312",
+			expected: "триста двенадцать ",
+		},
+		{
+			input:    "534",
+			expected: "пятьсот тридцать четыре ",
+		},
+		{
+			input:    "05",
+			expected: "пять ",
+		},
+		{
+			input:    "013",
+			expected: "тринадцать ",
+		},
+	}
+
+	for _, testCase := range testTable {
+		result := logic(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+		}
+	}
+}
+
+func TestLogicThousand(t *testing.T) {
+
+	testTable := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "312",
+			expected: "триста двенадцать ",
+		},
+		{
+			input:    "534",
+			expected: "пятьсот тридцать четыре ",
+		},
+		{
+			input:    "05",
+			expected: "пять ",
+		},
+		{
+			input:    "013",
+			expected: "тринадцать ",
+		},
+		{
+			input:    "022",
+			expected: "двадцать две ",
+		},
+		{
+			input:    "31",
+			expected: "тридцать одна ",
+		},
+	}
+
+	for _, testCase := range testTable {
+		result := logicThousand(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+		}
+	}
+}
+
+func TestAddEnding(t *testing.T) {
+	testTable := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "312",
+			expected: "ов ",
+		},
+		{
+			input:    "342",
+			expected: "а ",
+		},
+		{
+			input:    "551",
+			expected: " ",
+		},
+	}
+
+	for _, testCase := range testTable {
+		result := addEnding(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+
+		}
+	}
+}
+
+func TestAddEndingThousand(t *testing.T) {
+	testTable := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "361",
+			expected: "а ",
+		},
+		{
+			input:    "342",
+			expected: "и ",
+		},
+		{
+			input:    "558",
+			expected: " ",
+		},
+	}
+
+	for _, testCase := range testTable {
+		result := addEndingThousand(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+
+		}
+	}
+}
+
+func TestCompletePieces(t *testing.T) {
+	testTable := []struct {
+		input    []string
+		expected string
+	}{
+		{
+			input:    []string{"000", "43", "253", "452", "321"},
+			expected: "сорок три миллиарда двести пятьдесят три миллиона четыреста пятьдесят две тысячи триста двадцать один ",
+		},
+		{
+			input:    []string{"000", "1", "000", "000", "001"},
+			expected: "один миллиард один ",
+		},
+		{
+			input:    []string{"000", "000", "000", "000", "000"},
+			expected: "ноль",
+		},
+		{
+			input:    []string{"012", "334", "222", "731", "500"},
+			expected: "двенадцать триллионов триста тридцать четыре миллиарда двести двадцать два миллиона семьсот тридцать одна тысяча пятьсот ",
+		},
+	}
+	for _, testCase := range testTable {
+		result := completePieces(testCase.input)
+		t.Logf("Calling func with: %s, got result: %v ", testCase.input, result)
+
+		if !reflect.DeepEqual(result, testCase.expected) {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.expected, result)
+		}
+	}
+
+}
