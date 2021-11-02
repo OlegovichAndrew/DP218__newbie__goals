2a91d17890c92de91dba0ef3fb0735c29b0067b6
Andrew <ozzy.vob@mail.ru>
2021-11-02 23:29:07 +0200

    task7 + tests

--- task7/main.go
+++ task7/main.go
@@ -9,7 +9,7 @@ import (
 var limit int
 
 const instructions = "---------------------------------------------------------------\n" +
-	"Program will print squares of natural numbers until the limit\n" +
+	"Program will print the natural numbers which squares are lesser than the limit\n" +
 	"Set the limit with the flag -l\n" +
 	"Example:flag -l 255 sets the limit = 255\n" +
 	"---------------------------------------------------------------"
@@ -20,25 +20,20 @@ func adoptFlags() bool {
 	return limit > 0
 }
 
-func main()  {
-	if adoptFlags(){
-		printNaturalNumbers()
+func main() {
+	if adoptFlags() {
+		fmt.Println(printNaturalNumbers(limit))
 		return
 	}
 	fmt.Println(instructions)
 }
 
-func printNaturalNumbers() {
+func printNaturalNumbers(limit int) string {
 	var result []string
-	for i := 1; i < limit; i ++ {
-		if i * i < limit {
+	for i := 1; i < limit; i++ {
+		if i*i < limit {
 			result = append(result, fmt.Sprint(i))
 		}
 	}
-	fmt.Printf(strings.Join(result, ","))
-	fmt.Println()
+	return strings.Join(result, ",")
 }
-
-
-
-
